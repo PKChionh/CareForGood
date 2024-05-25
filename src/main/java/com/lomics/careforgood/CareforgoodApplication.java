560d7c6ff9a54de4d77b8d844c635af31c00678c
pkchionh <ite.chionh.puay.kiat@gmail.com>
2024-05-25 18:11:16 +0800

    Initial commit

---
+++ src/main/java/com/lomics/careforgood/CareforgoodApplication.java
@@ -0,0 +1,13 @@
+package com.lomics.careforgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareforgoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareforgoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lomics/careforgood/CareforgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lomics.careforgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareforgoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
